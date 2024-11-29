c3180e1cd4e81af13f8a90c90fb50ec0d2b5184a
Kappaccinoh <lim.jia28@gmail.com>
2024-11-29 15:28:46 +0800

    counter

---
+++ count.py
@@ -0,0 +1,13 @@
+import os
+
+def count_images(directory):
+    image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}
+    total_images = 0
+
+    for root, dirs, files in os.walk(directory):
+        total_images += sum(1 for file in files if os.path.splitext(file)[1].lower() in image_extensions)
+
+    return total_images
+
+directory = "done"  # Replace with the path to your "done" folder
+print(f"Total number of images: {count_images(directory)}")
